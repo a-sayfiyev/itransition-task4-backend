c34ea66f8edb4dc6ac38731d3910c5a487e2acd6
a-sayfiyev <akobirsayfiyev27@gmail.com>
2024-02-21 11:08:47 +0500

    Make multiple cors origins

--- index.js
+++ index.js
@@ -21,15 +21,23 @@ mongoose
 
 app.use(express.json());
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://manageus-sayfiyev.netlify.app"
+];
 
-const corsOptions = {
-  origin: "https://manageus-sayfiyev.netlify.app",
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
   credentials: true,
-  optionsSuccessStatus: 204,
-};
-
-app.use(cors(corsOptions));
+}));
 
 app.use("/api/user", authRoute);
 app.use("/api/control", userRoute);
